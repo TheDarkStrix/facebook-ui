a72de10547c69e2b89454978eb780f40104e8f21
TheDarkStrix <thedarkstrix@gmail.com>
2021-10-08 17:50:02 +0530

    Gif Search: Add search and debounce

--- src/components/createPost/gifRendered/gifRendered.js
+++ src/components/createPost/gifRendered/gifRendered.js
@@ -2,10 +2,13 @@ import { useState, useEffect } from "react";
 import "./gifRender.css";
 import axios from "axios";
 import Loading from "./loading/loading";
+import useDebounce from "./use-debounce";
 
 const GifRendered = () => {
   const [gifs, setGifs] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState(null);
+  const debouncedSearchTerm = useDebounce(search, 1000);
 
   const fetchGif = async () => {
     const response = await axios.get("http://api.giphy.com/v1/gifs/trending", {
@@ -14,25 +17,43 @@ const GifRendered = () => {
       },
     });
     if (response.data) {
-      console.log(response.data.data);
       setGifs(response.data.data);
       setLoading(false);
     }
   };
 
-  //   const fetchGif = async () => {
-  //     const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
-  //       params: {
-  //         q: "",
-  //         api_key: "Rr6XJXDmFfDfiqTHoptbSVVmf8lzQ1Jc",
-  //       },
-  //     });
-  //     if (response.data) {
-  //       console.log(response.data.data);
-  //       setGifs(response.data.data);
-  //       setLoading(false);
-  //     }
-  //   };
+  const searchGif = async (search) => {
+    const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+      params: {
+        q: search,
+        api_key: "Rr6XJXDmFfDfiqTHoptbSVVmf8lzQ1Jc",
+      },
+    });
+    if (response.data) {
+      setGifs(response.data.data);
+      setLoading(false);
+    }
+  };
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(
+    () => {
+      if (debouncedSearchTerm) {
+        setLoading(true);
+        searchGif(debouncedSearchTerm);
+      } else {
+        if (debouncedSearchTerm === "") {
+          fetchGif();
+        } else {
+          setGifs([]);
+        }
+      }
+    },
+    [debouncedSearchTerm] // Only call effect if debounced search term changes
+  );
 
   useEffect(() => {
     fetchGif();
@@ -44,6 +65,14 @@ const GifRendered = () => {
           <Loading />
         ) : (
           <div>
+            <div className="searchContainer">
+              <input
+                type="text"
+                onChange={handleSearch}
+                defaultValue={search == null ? "" : search}
+                placeholder="Search ..."
+              />
+            </div>
             {gifs && gifs.length > 0 ? (
               <div>
                 <ul className="gifList">
